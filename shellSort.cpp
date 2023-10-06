2b3e1e7f0ecc97643d5212559d25b4a3c1e485bf
bao-bk2019 <duongquanbao19962012@gmail.com>
2023-10-06 07:31:15 +0700

    shellshort

---
+++ shellSort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+// Modified version of Insertion Sort for varying increments
+template <typename E>
+void my_swap(E A[], int i, int j) {
+    std::swap(A[i], A[j]);
+}
+
+template <typename E, typename Comp>
+void inssort2(E A[], int n, int incr) {
+    for (int i = incr; i < n; i += incr)
+        for (int j = i; (j >= incr) && (Comp::prior(A[j], A[j - incr])); j -= incr)
+            my_swap(A, j, j - incr);
+}
+template <typename E, typename Comp>
+void shellsort(E A[], int n) { // Shellsort
+  for (int i=n/2; i>2; i/=2)      // For each increment
+    for (int j=0; j<i; j++)       // Sort each sublist
+      inssort2<E,Comp>(&A[j], n-j, i);
+  inssort2<E,Comp>(A, n, 1);
+}
+
+// Example comparison function
+struct CustomComparator {
+    static bool prior(int a, int b) {
+        return a < b; // Sort in ascending order
+    }
+};
+
+int main() {
+    int a[]={59, 20, 17, 13, 28, 14, 23, 83, 36, 98, 11, 70, 65, 41, 42, 15};    
+    int n = sizeof(a) / sizeof(a[0]);
+
+    // Call shellsort with the custom comparison function
+    shellsort<int, CustomComparator>(a, n);
+
+    // Output the sorted array
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
